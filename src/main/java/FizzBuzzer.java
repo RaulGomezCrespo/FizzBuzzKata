78439a2dfff08d191d7118e469d50b9684e266cb
Raul Gomez Crespo <rgomezcr@axpecantabria.com>
2021-09-28 19:07:36 +0200

    create test refactor method fizzBuzz

--- src/main/java/FizzBuzzer.java
+++ src/main/java/FizzBuzzer.java
@@ -3,15 +3,12 @@ public class FizzBuzzer {
 
 	public static String fizzBuzz(int number) {
 		
-		if(number == 1) {
-			return "1";
-		} else if(number == 2) {
-			return "2";
-		} else if(number == 3) {
+		if(number == 3) {
 			return "Fizz";
 		} else {
-			return "4";
+			return Integer.toString(number);
 		}
+		
 		
 	}
 
